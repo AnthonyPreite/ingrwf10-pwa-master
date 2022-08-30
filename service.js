ff34adcbb014467c38d48cffc4e474523fead49a
Astoryon <adressemail>
2022-08-30 10:19:45 +0200

    - stocking into the cache_storage ressources

--- service.js
+++ service.js
@@ -13,8 +13,94 @@ self.addEventListener('activate', event => {
     return self.clients.claim()
 })
 
+//simple fetch général pour le InstallBtn
+// on écoute chaque requête
+/*
 self.addEventListener('fetch', event => {
     const requestUrl = new URL(
         event.request.url
     )
+})
+*/
+
+// définition d'un cache pour les assets
+
+const ASSETS_CACHE_NAME = "assets"
+
+// 2 méthodes : getter et setter pour le cache
+
+// GETTER
+const getResponseFromCache = (
+    cacheName,
+    request
+) => {
+    // on ouvre le bon cache
+    return caches.open(cacheName)
+    // on récupère le cache
+    .then( cache => {
+        // on returne le fichier qui correspond à la requête
+        return cache.match(request)
+    })
+}
+
+const setResponseCache = (
+    cacheName,
+    request,
+    response
+) => {
+     // on ouvre le bon cache
+     return caches.open(cacheName)
+     .then(cache => {
+        return cache.put(request, response)
+     })
+
+}
+
+
+// stratégie de cache
+// Méthode de stratégie de cache first
+
+const getResponseFromCacheFirst = (
+    cacheName,
+    request
+) => {
+  //on exécute le getter en premier
+    const response = getResponseFromCache(
+      cacheName, request
+    )
+    .then( response => {
+      //est ce que la réponse existe dans le cache
+      if(response) {
+        // on retourne la réponse à la stratégie
+        return response
+      }
+      else { // le fichier correspondante à request n'est pas dans le cache
+        return fetch(request)     //requête sur le serveur asynchrone
+        .then( response => {
+          setResponseCache(
+            cacheName,
+            request,
+            response.clone()
+          )
+          // on envois la réponse a la stratégie
+          return response
+        }) 
+      }
+    })
+    // on renvoie au fetch
+    return response
+}
+// si request, on l'intercept
+self.addEventListener("fetch", event => {
+  //on récupère l'url de la requête éxécutée par le navigateur
+  const requestUrl = new URL(
+    event.request.url
+  )
+  // on intercepte la requête et on applique la stratégie cacheFirst
+  if(requestUrl.pathname.startsWith('/assets')){
+    // répond au navigateur avec le résultat de la stratégie
+    event.respondWith(
+      getResponseFromCacheFirst(ASSETS_CACHE_NAME, event.request)
+    )
+  }
 })
